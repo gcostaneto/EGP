b5cc3a7e09730ae0fc0280d63212bd1cee3a2acb
Germano Costa Neto <germano.cneto@gmail.com>
2021-06-01 01:07:55 -0300

    Add files via upload

---
+++ supplementary_codes_2.R
@@ -0,0 +1,22 @@
+N_GE <- function(Kernel,         # some genomic, enviromic or enviromic x genomic kernel
+                 fraction =.98,  # expected fraction explained (Misztal, 2016)
+                 plot=TRUE,      # plot  svd?
+                 svd.print=FALSE # save svd?
+                 ){
+  svd.KERNEL <- svd(Kernel, nu = nrow(Kernel), nv = ncol(Kernel))
+  SVdD<-cumsum((svd.KERNEL$d[1:ncol(Kernel)])^2/sum(svd.KERNEL$d^2))
+  N <- length(SVdD[which(SVdD < fraction)])
+  if(isTRUE(plot)){
+    plot(SVdD,lwd=2,pch=15,xlab='Individuals',bty='l', cex.lab=1.5, cex.axis=1.5, cex.main=2.2,
+         main='Effective N size',ylab='Fraction of Kernel Variance')
+    lines(SVdD,lwd=2)
+    abline(v = N,lwd=3,col='red')
+  }
+  
+  cat(paste0('--------------------------------------','\n'))
+  cat(paste0('Explained variance ',100*fraction,'% \n'))
+  cat(paste0('Effective Ne:',N,'\n'))
+  cat(paste0('--------------------------------------','\n'))
+  if(isFALSE(svd.print)) return(list(Ne=N))
+  if(isTRUE(svd.print)) return(list(Ne=N,svd.d = svd.KERNEL$d,svd.u=svd.KERNEL$u,svd.v=svd.KERNEL$v))
+}
